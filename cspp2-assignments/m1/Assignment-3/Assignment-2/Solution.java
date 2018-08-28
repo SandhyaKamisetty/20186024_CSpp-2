6c17fea5b931faa53a693aeca4789eb3c7e7d747
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-28 20:23:19 +0530

    area

---
+++ cspp2-assignments/m1/Assignment-3/Assignment-2/Solution.java
@@ -0,0 +1,27 @@
+/*
+@author : SandhayKamisetty
+*/
+import java.util.Scanner;
+/*
+import util
+*/
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	
+	@param      args  The arguments
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int base = scan.nextInt();
+		int height = scan.nextInt();
+		double area = (0.5 * base * height);
+		System.out.println(area);
+
+	}
+	/*
+	Need to fill the areaOfTriangle function and print the output
+	of fahrenheit.
+	*/
+}
