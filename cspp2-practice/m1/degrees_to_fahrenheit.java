91873247c63ca49fc96574f7096f3edf2935c541
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-28 12:00:18 +0530

    quotient_remainder

---
+++ cspp2-practice/m1/degrees_to_fahrenheit.java
@@ -0,0 +1,7 @@
+public class degrees_to_fahrenheit{
+	public static void main(String[] args){
+		int t = 8;
+		double f = ((t * 1.8) + 32);
+		System.out.println(f);
+	}
+}
---
+++ cspp2-practice/m1/quotient_remainder.java
@@ -0,0 +1,12 @@
+public class quotient_remainder{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 6;
+		int c = a/b;
+		int d = a%b;
+		System.out.println(c + " " + d);
+		
+
+
+	}
+}
