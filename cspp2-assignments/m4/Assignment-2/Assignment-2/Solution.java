08789d8aee5f9599251ae5d802e1e89f101636f5
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-30 23:42:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -20,7 +20,7 @@ public final class Solution {
         Scanner sc = new Scanner(System.in);
         int r1 = sc.nextInt();
         int c1 = sc.nextInt();
-        int arr1[][] = new int[r1][c1];
+        int[][] arr1 = new int[r1][c1];
         for (int nr = 0; nr < r1; nr++) {
             for (int nc = 0; nc < c1; nc++) {
                 arr1[nr][nc] = sc.nextInt();
@@ -28,7 +28,7 @@ public final class Solution {
         }
         int r2 = sc.nextInt();
         int c2 = sc.nextInt();
-        int arr2[][] = new int[r2][c2];
+        int[][] arr2 = new int[r2][c2];
         for (int nr = 0; nr < r2; nr++) {
             for (int nc = 0; nc < c2; nc++) {
                 arr2[nr][nc] = sc.nextInt();
@@ -37,7 +37,7 @@ public final class Solution {
 
         }
         if(r1 == r2 && c1 == c2) {
-            int sum[][] = new int[r1][c1];
+            int[][] sum = new int[r1][c1];
             for (int nr = 0; nr < r1; nr++) {
                 for (int nc = 0; nc < c1; nc++) {
                     sum[nr][nc] = arr1[nr][nc] + arr2[nr][nc];
@@ -48,7 +48,7 @@ public final class Solution {
 
             for (int nr = 0; nr < r1; nr++) {
                 for (int nc = 0; nc < r1; nc++) {
-                    if(nc == r1 - 1) {
+                    if (nc == r1 - 1) {
                         System.out.print(sum[nr][nc]);
                     } else {
                         System.out.print(sum[nr][nc] + " ");
@@ -59,5 +59,6 @@ public final class Solution {
         } else {
             System.out.println("not possible");
         }
-        }   
+        }
     }
+
