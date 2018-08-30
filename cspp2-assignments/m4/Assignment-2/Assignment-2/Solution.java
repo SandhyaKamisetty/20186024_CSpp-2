a729ea1ff030327c86af69a2dc20d4f7ed442f35
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-30 23:40:33 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 /**
  * Do not modify this main function.
  * @author     : SandhyaKamisetty
@@ -30,17 +30,17 @@ public final class Solution {
         int c2 = sc.nextInt();
         int arr2[][] = new int[r2][c2];
         for (int nr = 0; nr < r2; nr++) {
-            for (int nc = 0; nc <c2; nc++) {
+            for (int nc = 0; nc < c2; nc++) {
                 arr2[nr][nc] = sc.nextInt();
 
             }
 
         }
-        if(r1==r2 && c1==c2) {
+        if(r1 == r2 && c1 == c2) {
             int sum[][] = new int[r1][c1];
             for (int nr = 0; nr < r1; nr++) {
-                for (int nc = 0; nc <c1; nc++) {
-                    sum[nr][nc] = arr1[nr][nc] + arr2[nr][nc]; 
+                for (int nc = 0; nc < c1; nc++) {
+                    sum[nr][nc] = arr1[nr][nc] + arr2[nr][nc];
 
                 }
 
@@ -48,23 +48,16 @@ public final class Solution {
 
             for (int nr = 0; nr < r1; nr++) {
                 for (int nc = 0; nc < r1; nc++) {
-                    if(nc==r1-1) {
+                    if(nc == r1 - 1) {
                         System.out.print(sum[nr][nc]);
-                    }
-                    else {
-                        System.out.print(sum[nr][nc]+" ");
+                    } else {
+                        System.out.print(sum[nr][nc] + " ");
                     }
                 }
                 System.out.println("");
             }
-        }
-        else {
+        } else {
             System.out.println("not possible");
         }
-
-
-        }
-        
-        
+        }   
     }
-
