41b0aa810957815d7e88223684258643493d3a63
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-30 23:30:21 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,57 +1,70 @@
+import java.util.*;
 /**
- * @author: SandhyaKamisetty.
- * Addition of matrices
- */
-import java.util.Scanner;
-/**
- * Class for solution.
+ * Do not modify this main function.
+ * @author     : SandhyaKamisetty
  */
 public final class Solution {
     /**
-     * Constructs the object.
-     */
+    * Fill the main function to print resultant of addition of matrices
+    */
     private Solution() {
-        /**
-         * unused constructor.
-         */
+    //We are not using this constructor.
     }
+
     /**
-     * main function.
+     * { function_description }.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner inp = new Scanner(System.in);
-        int row1 = Integer.parseInt(inp.next());
-        int col1 = Integer.parseInt(inp.next());
-        int[][] arr1 = new int[row1][col1];
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col1; j++) {
-                arr1[i][j] = Integer.parseInt(inp.next());
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int arr1[][] = new int[r1][c1];
+        for (int nr = 0; nr < r1; nr++) {
+            for (int nc = 0; nc < c1; nc++) {
+                arr1[nr][nc] = sc.nextInt();
             }
         }
-        int row2 = Integer.parseInt(inp.next());
-        int col2 = Integer.parseInt(inp.next());
-        int[][] arr2 = new int[row2][col2];
-        for (int i = 0; i < row2; i++) {
-            for (int j = 0; j < col2; j++) {
-                arr2[i][j] = Integer.parseInt(inp.next());
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int arr2[][] = new int[r2][c2];
+        for (int nr = 0; nr < r2; nr++) {
+            for (int nc = 0; nc <c2; nc++) {
+                arr2[nr][nc] = sc.nextInt();
+
             }
+
         }
-        if (row1 == row2 && col1 == col2) {
-            for (int i = 0; i < row2; i++) {
-            for (int j = 0; j < col2; j++) {
-                if (j != col2 - 1) {
-                    System.out.print(arr1[i][j] + arr2[i][j] + " ");
-                } else {
-                    System.out.print(arr1[i][j] + arr2[i][j]);
+        int sum[][] = new int[r1][c1];
+        for (int nr = 0; nr < r1; nr++) {
+            for (int nc = 0; nc <c1; nc++) {
+                sum[nr][nc] = arr1[nr][nc] + arr2[nr][nc]; 
+
+            }
+
+        }
+
+        if(r1==r2 && c1==c2) {
+            for (int nr = 0; nr < r1; nr++) {
+                for (int nc = 0; nc < r1; nc++) {
+                    if(nc==r1-1) {
+                        System.out.print(sum[nr][nc]);
+                    }
+                    else {
+                        System.out.print(sum[nr][nc]+" ");
+                    }
                 }
-                // String b = (j != col2 - 1) ? " " : "";
+                System.out.println("");
             }
-            System.out.println("");
         }
-        } else {
+        else {
             System.out.println("not possible");
         }
+
+
+        }
+        
+        
     }
-}
+
