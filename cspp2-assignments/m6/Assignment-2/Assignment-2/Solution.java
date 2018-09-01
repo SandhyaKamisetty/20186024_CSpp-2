4508fbceb0b065e1c8280958e3bfedae261bb5b1
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-09-01 12:52:02 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Assignment-2/Solution.java
@@ -1,11 +1,50 @@
 import java.util.Scanner;
 /**
- * Write a java program to round the
- * elements of a matrix to the nearest 100.
+ * Write a java program to round the elements of a matrix to the nearest 100.
  *
- * @author : 
+ * @author     :
  */
 final class Solution {
+    /**.
+        *Do not modify this main function.
+    */
+    public static final int A = 50;
+    /**.
+     *to check.
+    */
+    public static final int B = 100;
+    /**.
+     *to check.
+    */
+    public static final int C = 150;
+    /**.
+     *to check.
+    */
+    public static final int D = 200;
+    /**.
+     *to check.
+    */
+    public static final int E = 250;
+    /**.
+     *to check.
+    */
+    public static final int F = 300;
+    /**.
+     *to check.
+    */
+    public static final int G = 350;
+    /**.
+     *to check.
+    */
+    public static final int H = 400;
+    /**.
+     *to check.
+    */
+    public static final int I = 450;
+    /**.
+     *to check.
+    */
+    public static final int J = 500;
     /**
      * Constructs the object.
      */
@@ -25,28 +64,28 @@ final class Solution {
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                if (a[i][j] < 50) {
+                if (a[i][j] < A) {
                     a[i][j] = 0;
-                } else if (a[i][j] > 50 || a[i][j] < 100) {
-                    a[i][j] = 100;
-                } else if (a[i][j] > 100) {
-                    a[i][j] = 200;
-                } else if (a[i][j] > 150 && a[i][j] < 200) {
-                    a[i][j] = 200;
-                } else if (a[i][j] > 200 && a[i][j] < 250) {
-                    a[i][j] = 200;
-                } else if (a[i][j] > 250 || a[i][j] < 300) {
-                    a[i][j] = 300;
-                } else if (a[i][j] > 300 || a[i][j] < 350) {
-                    a[i][j] = 300;
-                } else if (a[i][j] > 350 || a[i][j] < 400) {
-                    a[i][j] = 400;
-                } else if (a[i][j] > 400 || a[i][j] < 450) {
-                    a[i][j] = 400;
-                } else if (a[i][j] > 450 || a[i][j] < 400) {
-                    a[i][j] = 400;
+                } else if (a[i][j] > A || a[i][j] < B) {
+                    a[i][j] = B;
+                } else if (a[i][j] > B) {
+                    a[i][j] = D;
+                } else if (a[i][j] > C && a[i][j] < D) {
+                    a[i][j] = D;
+                } else if (a[i][j] > D && a[i][j] < E) {
+                    a[i][j] = D;
+                } else if (a[i][j] > E || a[i][j] < F) {
+                    a[i][j] = F;
+                } else if (a[i][j] > F || a[i][j] < G) {
+                    a[i][j] = F;
+                } else if (a[i][j] > G || a[i][j] < H) {
+                    a[i][j] = H;
+                } else if (a[i][j] > H || a[i][j] < I) {
+                    a[i][j] = H;
+                } else if (a[i][j] > I || a[i][j] < H) {
+                    a[i][j] = I;
                 } else {
-                    a[i][j] = 500;
+                    a[i][j] = J;
                 }
             } 
         } return a;
