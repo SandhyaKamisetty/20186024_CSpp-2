960a30c8980d2888731c40077286b473a2d97d9d
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-09-15 12:54:31 +0530

    "check_style_score -> 5 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Assignment-1/Solution.java
@@ -80,7 +80,7 @@ public final class Solution {
             System.out.println("Error! Malformed question");
             return;
         }
-        if (token.length < 5) {
+        if (token.length < (2+2+1)) {
             System.out.println("Error! Malformed question");
             return;
         }
